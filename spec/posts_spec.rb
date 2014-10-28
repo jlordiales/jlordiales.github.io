69194019bbfc2bdcf4706d929383c614f50166d8
Jose Luis Ordiales Coscia <jlordiales@gmail.com>
2014-10-28 19:05:10 -0200

    Formatting rspec expectations

--- spec/posts_spec.rb
+++ spec/posts_spec.rb
@@ -28,27 +28,33 @@ describe "Posts" do
 
 
   def has_comments_enabled?(post_file)
-    expect(front_matter(post_file)["comments"]).to eq(true), "Post #{post_file} does not have comments enabled"
+    expect(front_matter(post_file)["comments"]).to eq(true), 
+      "Post #{post_file} does not have comments enabled"
   end
 
   def has_sharing_enabled?(post_file)
-    expect(front_matter(post_file)["share"]).to eq(true), "Post #{post_file} does not have sharing enabled"
+    expect(front_matter(post_file)["share"]).to eq(true), 
+      "Post #{post_file} does not have sharing enabled"
   end
 
   def uses_post_layout?(post_file)
-    expect(front_matter(post_file)["layout"]).to eq("post"), "Post #{post_file} does not have a post layout"
+    expect(front_matter(post_file)["layout"]).to eq("post"), 
+      "Post #{post_file} does not have a post layout"
   end
 
   def author_equals_to(post_file, author)
-    expect(front_matter(post_file)["author"]).to eq(author), "Post #{post_file} does not have an author equal to #{author}"
+    expect(front_matter(post_file)["author"]).to eq(author), 
+      "Post #{post_file} does not have an author equal to #{author}"
   end
 
   def has_a_publication_date?(post_file)
-    expect(front_matter(post_file)["date"]).to_not be_nil, "Post #{post_file} does not have a publication date"
+    expect(front_matter(post_file)["date"]).to_not be_nil, 
+      "Post #{post_file} does not have a publication date"
   end
 
   def has_a_title?(post_file)
-    expect(front_matter(post_file)["title"]).to_not be_nil, "Post #{post_file} does not have a title"
+    expect(front_matter(post_file)["title"]).to_not be_nil, 
+      "Post #{post_file} does not have a title"
   end
 
   def front_matter(post_file)
